3d7f9c82201fc99ca8838f147153f9d3ddcf0cc5
unknown <abdallahashraf203@gmail.com>
2025-02-22 00:52:58 +0200

    to lmdb file 6

--- scripts/data_preparation/utils/utils_lmdb.py
+++ scripts/data_preparation/utils/utils_lmdb.py
@@ -13,7 +13,7 @@ def make_lmdb_from_imgs(data_path,
                         batch=5000,
                         compress_level=1,
                         multiprocessing_read=False,
-                        n_thread=16,
+                        n_thread=4,
                         map_size=None):
     """Make lmdb from images.
 
